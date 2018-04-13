6596f7369fb1e2c067fc91d905b96c877b897248
warlock921 <caoyu921@163.com>
2018-04-14 01:54:55 +0800

    分页优化

--- mainsite/views.py
+++ mainsite/views.py
@@ -19,11 +19,11 @@ def blog_article_count():
         blog_types_dict[blog_types_kind[i]] = blog_type_count
     return blog_types_dict
 
-#分页方法，需要传入request和每页需要的文章数
-def page_2_page(request, num_of_page):
-    all_blogs = Blog.objects.all()
+#分页方法，需要传入request、每页需要的文章数、用于分页的文章总集合
+def page_2_page(request, num_of_page, need_blogs):
+    # all_blogs = Blog.objects.all()
     #分页处理
-    paginator = Paginator(all_blogs, num_of_page) #分页处理，num_of_page代表每页的文章数
+    paginator = Paginator(need_blogs, num_of_page) #分页处理，num_of_page代表每页的文章数
     #获取页面传来参数（第N页）
     page = request.GET.get('page')
     page_num = paginator.get_page(page)
@@ -45,6 +45,7 @@ def page_2_range(page_num):
         page_range.insert(0,"...")
     if page_num.paginator.num_pages - page_range[-1] >= 2 :
         page_range.append("...")
+        
 
     #判断页码，添加第一页和最后一页
     if page_range[0] != 1:
@@ -56,7 +57,7 @@ def page_2_range(page_num):
 
 def blog_list(request):
     all_blogs = Blog.objects.all()
-    page_num = page_2_page(request, 4) #调用分页方法，4 - 代表每页的文章数
+    page_num = page_2_page(request, 4, all_blogs) #调用分页方法，4 - 代表每页的文章数
     page_range = page_2_range(page_num) #调用页码缩减方法，传入的是分页后的数据
     context = {}
     context['blog_list'] = all_blogs
@@ -77,10 +78,19 @@ def blog_detail(request, blog_pk):
 def blog_type_selected(request, blog_type_pk):
     context = {}
     blog_type = get_object_or_404(BlogType,pk=blog_type_pk)
+    blog_detail_selected = Blog.objects.filter(blog_type=blog_type)
+    #分页
+    page_num = page_2_page(request, 4, blog_detail_selected) #调用分页方法，4 - 代表每页的文章数
+    page_range = page_2_range(page_num) #调用页码缩减方法，传入的是分页后的数据
+
     context['blog_all_list'] = Blog.objects.all()
-    context['blog_detail_selected'] = Blog.objects.filter(blog_type=blog_type)
+    # context['blog_detail_selected'] = blog_detail_selected
     context['blog_type'] = blog_type
     context['blog_types'] = BlogType.objects.all()
     context['blog_types_dict'] = blog_article_count() #调用博客文章计数方法
+    #传递分页后的blog文章集合信息
+    context['page_num'] = page_num
+    #缩减后的页码范围
+    context['page_range'] = page_range
     
     return render_to_response('mainsite/blog_type_selected.html', context)
