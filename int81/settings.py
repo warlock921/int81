0f81ef98260d18262b4a9a08ee731fe2e77d38b2
warlock921 <caoyu921@163.com>
2018-06-10 22:40:38 +0800

    服务器简单修改页面元素

--- int81/settings.py
+++ int81/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@o(*2q%#)dulz^_mnc%$3k23k!(t(5up6jp#=5xw-_^p+x(8v='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost","127.0.0.1","0.0.0.0","www.int81.com","int81.com"]
 
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'int81',
         'USER': 'root',
-        'PASSWORD': 'P@ssword',
+        'PASSWORD': 'ltjg810921',
         "HOST": "localhost",
     }
 }
@@ -131,8 +131,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static"),
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR,"static/")
-
+STATIC_ROOT = [
+    os.path.join(BASE_DIR,"static"),	
+]
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',  
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',  
